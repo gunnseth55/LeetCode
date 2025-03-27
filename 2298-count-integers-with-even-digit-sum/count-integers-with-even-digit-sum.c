e76639545411c5fb3e4b1fafc159f8e480181b51
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-03-28 00:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (2.56%) - LeetSync

---
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.c
@@ -0,0 +1,17 @@
+int countEven(int num) {
+    int count=0;
+    for(int i=1;i<=num;i++){
+        
+        int x=i;
+        int sum=0;
+        while(x!=0){
+            sum+=(x%10);
+            x=x/10;
+        }
+        if(sum%2==0){
+            count++;
+        }
+        
+    }
+    return count;
+}
