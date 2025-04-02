77ac121788fd13d7da3e0ee671532206836e3e57
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-04-02 20:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (94.68%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.c
@@ -0,0 +1,10 @@
+int commonFactors(int a, int b) {
+   int count=0;
+   int small=a<b?a:b;
+   for(int i=1;i<=small;i++){
+    if(a%i==0 && b%i==0){
+        count++;
+    }
+   }
+   return count;
+}
