a57409d294c677baaf5eb9f8cb9130da17b742b3
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-06-26 17:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (57.76%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,44 +1,53 @@
-
-
 class Solution {
+    
 public:
-    bool isnum(char c) {
-        return c >= '0' && c <= '9';
-    }
 
+ bool isnum(char c){
+    return c>='0'&& c<='9';
+ }
     int calculate(string s) {
-        stack<int> nums;
-        stack<int> signs;
-        int num = 0, result = 0, sign = 1;
+        stack<int> number;
+        stack<int>signn;
+        long num=0;
+        int res=0;
+        int sign=1;
+        for(int i=0;i<s.length();i++){
 
-        for (int i = 0; i < s.size(); i++) {
-            char c = s[i];
-
-            if (isnum(c)) {
-                num = num * 10 + (c - '0');
-            } else if (c == '+') {
-                result += sign * num;
-                num = 0;
-                sign = 1;
-            } else if (c == '-') {
-                result += sign * num;
-                num = 0;
-                sign = -1;
-            } else if (c == '(') {
-                // Push current result and sign
-                nums.push(result);
-                signs.push(sign);
-                result = 0;
-                sign = 1;
-            } else if (c == ')') {
-                result += sign * num;
-                result *= signs.top(); signs.pop();
-                result += nums.top(); nums.pop();
-                num = 0;
+            char c=s[i];
+            if(isnum(c)){
+                num=(num*10)+(c-'0');
             }
-        }
+            else if(c=='+'){
+                res+=sign*num;
+                sign=1;
+                num=0;
+            }else if(c=='-'){
+                res+=sign*num;
+                num=0;
+                sign=-1;
+            }else if(c=='('){
+                number.push(res);
+                signn.push(sign);
+                sign=1;
+                num=0;
+                res=0;
 
-        result += sign * num;
-        return result;
+            }else if(c==')'){
+                res+= num*sign;
+                res= number.top()+signn.top()*res;
+                number.pop();
+
+                
+                signn.pop();
+                num=0;
+                
+            }
+
+
+        }
+        res+=num*sign;
+        return res;
     }
+    
+
 };
