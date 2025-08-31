d824f87e4882562135bb65a2a6e0aace625af08d
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-08-31 12:46:52 +0530

    Time: 524 ms (5.03%) | Memory: 339.5 MB (5.11%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-bool isPalindrome(string s){
-    string x=s;
-    reverse(s.begin(),s.end());
-    return (x==s);
-}
- vector<vector<string>> solve(  string &s){
-if(s.empty())return{{}};
-
-vector<vector<string>>result;
-
-for(int i=1;i<=s.size();i++){
-    string sub1=s.substr(0,i);
-    string sub2=s.substr(i);
-
-if(isPalindrome(sub1)){
-    vector<vector<string>>sub=solve(sub2);
-    for(auto &p:sub){
-        vector<string>temp;
-        temp.push_back(sub1);
-        temp.insert(temp.end(),p.begin(),p.end());
-        result.push_back(temp);
+    bool isPalindrome(const string &s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
     }
 
-}
- }
- return result;
- }
+    vector<vector<string>> solve(const string &s) {
+        if (s.empty()) return { {} };  // base case: one empty partition
 
-
+        vector<vector<string>> result;
+        for (int i = 1; i <= s.size(); i++) {
+            string prefix = s.substr(0, i);
+            if (isPalindrome(prefix)) {
+                string rest = s.substr(i);
+                vector<vector<string>> sub = solve(rest);
+                for (auto &p : sub) {
+                    vector<string> temp = {prefix};
+                    temp.insert(temp.end(), p.begin(), p.end());
+                    result.push_back(temp);
+                }
+            }
+        }
+        return result;
+    }
 
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>result;
-        if(s.empty())return result;
-      if(s.length()==1)return {{s}};
-       result= solve(s);
-        return result;
+        return solve(s);
     }
 };
