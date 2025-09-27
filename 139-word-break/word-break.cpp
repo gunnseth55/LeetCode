15d3f6c8cdd6f5487334fb58c4de11d42a91c1c8
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-09-27 12:23:46 +0530

    Time: 2 ms (75.79%) | Memory: 12.9 MB (64.66%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-bool solve(string &s, vector<string>&wordDict , int start,vector<int> &dp){
-
-    if(start==s.size())return true;
-   if(dp[start]!=-1)return dp[start];
-    for(int i=0;i<wordDict.size();i++){
-        int n =wordDict[i].size();
-         if (start + n > s.size()) continue;
-       string temp=s.substr(start,n);
-       if(temp==wordDict[i]){
-       
-     if(solve(s,wordDict,n+start,dp)){
-        
-       return  dp[start]= true;
-     }
-       }
+bool solve(string s, vector<string>&wordDict, int start, vector<int>&dp){
+if(start==s.size())return true;
+if(dp[start]!=-1)return dp[start];
+for(int i=0;i<wordDict.size();i++){
+    int n=wordDict[i].size();
+    if((start+n)>s.size()){
+        continue;
     }
-    return dp[start]=false;
+    string temp=s.substr(start,n);
+    if(temp==wordDict[i]){
+        if(solve(s,wordDict,start+n,dp)){
+            return dp[start]=true;
+        }
+    }
 }
-  
 
+return dp[start]=false;
+}
     bool wordBreak(string s, vector<string>& wordDict) {
-      vector<int>dp(s.size(),-1);
-       return solve(s,wordDict,0,dp);
-
-
+        vector<int>dp(s.size(),-1);
+        return solve(s,wordDict,0,dp);
     }
 };
