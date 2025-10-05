20aa050450e5088609aad1dee863ea8911ba8941
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-10-06 00:02:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (81.53%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-void solve(vector<vector<int>>&result,vector<int>&temp, vector<int> &arr,int k, int index, int target){
+void solve(vector<vector<int>>&result, vector<int>&arr, vector<int>&temp,int k,int n, int index){
     int sum=accumulate(temp.begin(),temp.end(),0);
-  if(temp.size()==k && sum==target){
-    result.push_back(temp);
-    return;
-  }
-  for(int i=index;i<arr.size();i++){
-    temp.push_back(arr[i]);
-    solve(result,temp,arr,k,i+1,target);
-    temp.pop_back();
-  }
-  
+    if(sum==n && temp.size()==k){
+        result.push_back(temp);
+        return;
+    }
+    for(int i=index;i<arr.size();i++){
+        temp.push_back(arr[i]);
+        solve(result,arr,temp,k,n,i+1);
+        temp.pop_back();
+    }
 }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>result;
         if(k>n)return result;
-        vector<int>arr={1,2,3,4,5,6,7,8,9};
         vector<int>temp;
-        
-        solve(result,temp,arr,k,0,n);
-        
+        vector<int>arr={1,2,3,4,5,6,7,8,9};
+        solve(result,arr,temp,k,n,0);
         return result;
     }
 };
