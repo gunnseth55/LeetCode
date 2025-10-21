f5d7016425e482856c953f0fb2b85f429f7abc33
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-10-22 01:52:57 +0530

    Time: 56 ms (9.11%) | Memory: 125.1 MB (81.61%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -4,22 +4,19 @@ public:
         int n=spells.size();
         int m=potions.size();
         vector<int>result(n,0);
-
         sort(potions.begin(),potions.end());
         for(int i=0;i<n;i++){
-            bool flag=false;
             int start=0,end=m-1;
-        while(start<=end ){
-            int mid=start+(end-start)/2;
-            long long prod=(long long)spells[i]*(long long)potions[mid];
-            if(prod<success){
-                start=mid+1;
-            }else if(prod>=success){
-                end=mid-1;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                long long prod=(long long)spells[i]*(long long)potions[mid];
+                if(prod>=success){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
             }
-           
-        }
-         result[i]=m-start;
+            result[i]=m-start;
         }
         return result;
     }
