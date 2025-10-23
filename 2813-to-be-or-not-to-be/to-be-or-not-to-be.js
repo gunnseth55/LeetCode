6a4038c3ecb6d2d8dc5bb53e84fb0c1dbfd952e2
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-10-23 23:38:22 +0530

    Time: 47 ms (48.10%) | Memory: 54.3 MB (20.79%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+let x={
+   toBe: function(x){
+        if(x===val)return true;
+        else throw new Error("Not Equal");
+    },
+    notToBe: function(y){
+        if(y!==val)return true;
+        else throw new Error("Equal");
+    }
+};
+return x
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
