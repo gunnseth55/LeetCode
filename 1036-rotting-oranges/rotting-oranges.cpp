d9055500349d884c97dc55f055e3bf152abce040
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-11-11 19:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (87.72%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int row=grid.size(),col=grid[0].size();
         queue<pair<int,int>>q;
         int onec=0;
-        int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
-        int count=0,i,j;
-        for(i=0;i<row;i++){
-            for(j=0;j<col;j++){
+        int row=grid.size();
+        int col=grid[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
-                    
-                } if(grid[i][j]==1){
+                }else if(grid[i][j]==1){
                     onec++;
                 }
             }
-        } if(onec==0)return 0;
+        }
+        vector<pair<int,int>>dir={{+1,0},{0,-1},{-1,0},{0,+1}};
+        int count=0;
+        if (onec==0)return 0;
         while(!q.empty()){
-            int s=q.size();
+            int n=q.size();
             count++;
-            for(int k=0;k<s;k++){
-                auto  [a,b]=q.front();q.pop();
-                for(auto &d:dir){
-                    int newi=d[0]+a,newj=d[1]+b;
-                    if(newi>=0 && newj>=0 && newi<row&& newj<col && grid[newi][newj]==1){
-                        grid[newi][newj]=2;
-                        q.push({newi,newj});
+            for(int i=0;i<n;i++){
+               auto [a,b]=q.front(); q.pop();
+                for(auto d:dir){
+                    int na=a+d.first,nb=b+d.second;
+                    if(na>=0 && nb>=0 && na<row && nb<col && grid[na][nb]==1)
+                    {
+                        grid[na][nb]=2;
+                        q.push({na,nb});
                     }
+
                 }
             }
+
+
         }
         onec=0;
-        for(int a=0;a<row;a++){
-            for(int b=0;b<col;b++){
-                if(grid[a][b]==1){
-                    onec++;
-                }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                 if(grid[i][j]==1)   onec++;
             }
         }
-       return onec==0?count-1:-1;
+        return onec==0?count-1:-1;
     }
 };
