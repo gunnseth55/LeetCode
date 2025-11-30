6c7e356503954a0db25413bcb700cde0e22acf7c
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-11-30 14:22:34 +0530

    Time: 7 ms (88.83%) | Memory: 9.8 MB (13.64%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
-        
-
-        int n=s.length();
-        int left=0,right=s.size()-1,ans=0;
-        while(left<right){
-            int l=left,r=right;
-            while(s[l]!=s[r])r--;
-            if(l==r){
-                swap(s[r],s[r+1]);
-                ans++;
-                continue;
-            }else{
-                while(r<right)swap(s[r],s[r+1]),ans++,r++;
-            }
-            left++,right--;
-
-
+     int n=s.length();
+     int left=0,right=n-1;
+     int ans=0;
+     while(left<right){
+        int l=left,r=right;
+        while(s[l]!=s[r])r--;
+        if(l==r){
+            swap(s[r],s[r+1]);
+            ans++;
+            continue;
+        }else{
+           while(r<right)
+            swap(s[r],s[r+1]), r++, ans++;
+           
         }
-        return ans;
+        left++,right--;
+     }   
+    return ans;
     }
 };
