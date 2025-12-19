c24c1f02f39f733a7719c73114fe4102b0f9c8ee
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2025-12-19 17:39:31 +0530

    Time: 4 ms (62.54%) | Memory: 13.5 MB (16.36%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        
+        int nn=nums.size();
         int i=0;
-        int mid=(2*n)/2;
+        int mid=nn/2;
     
         int j=mid;
         vector<int>result;
-        while(i<mid && j<(2*n) ){
+        while(i<mid && j<nn ){
             result.push_back(nums[i]);
             result.push_back(nums[j]);
             i++;
