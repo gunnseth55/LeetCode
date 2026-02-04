2b20672be5e6fcefd29b7477bd2b683b9f187abf
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2026-02-04 20:13:02 +0530

    Time: 6 ms (55.19%) | Memory: 13.8 MB (7.08%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     int romanToInt(string s) {
         int result=0;
-        
-        if(s.length()==0)
-        return result;
-
-        map<char,int>m={
-            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
-        };
+        if(s.length()==0){
+            return result;
+        }
+        map<char,int>m={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',  1000}};
         for(int i=0;i<s.length();i++){
             int curr=m[s[i]];
-            int next=0;
-            if(i+1<s.length()){
-               next= m[s[i+1]];
-            }
-            if(curr<next){
-                result-=curr;
-            }else if(curr>=next){
+            int next=m[s[i+1]];
+            if(next<=curr){
                 result+=curr;
+            }else{
+                result-=curr;
             }
         }
         return result;
