6c11e649b0e6bc99cf2bc4c6721fc99f77105f0d
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2026-04-26 23:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (71.82%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+       map<int,int>m;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]==0)continue;
+        m[nums[i]]=i;
+       }
+       return m.size();
+    }
+};
