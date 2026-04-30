ca8c8424b509367b992689f3ad5112a7ab64fbb6
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2026-05-01 00:08:25 +0530

    Time: 20 ms (84.25%) | Memory: 61.5 MB (50.10%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,14 +4,14 @@ public:
         priority_queue<int,vector<int>,greater<int>>pq;
         for(int i=0;i<k;i++){
             pq.push(nums[i]);
-
         }
         for(int i=k;i<nums.size();i++){
-            if(nums[i]>pq.top()){
+            if(pq.top()<nums[i]){
                 pq.pop();
                 pq.push(nums[i]);
+                
             }
         }
-    return pq.top();
+        return pq.top();
     }
 };
