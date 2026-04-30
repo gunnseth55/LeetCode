58e691c9e0a523170613ad1d0447158cba56335d
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2026-05-01 00:19:09 +0530

    Time: 20 ms (94.66%) | Memory: 16.2 MB (77.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void solve(int x,int y,vector<vector<char>>& grid,int row,int col){
-       
-        if(x>=row ||y>=col ||x<0 ||y<0||grid[x][y]=='0')return;
-        grid[x][y]='0';
-            solve(x+1,y,grid,row,col);
-            solve(x-1,y,grid,row,col);
-            solve(x,y+1,grid,row,col);
-            solve(x,y-1,grid,row,col);
+    void solve(vector<vector<char>>&grid, int row, int col,int i, int j){
+        if(i==row||j==col||i<0||j<0||grid[i][j]=='0')return;
+
+        grid[i][j]='0';
+        solve(grid,row, col,i-1,j);
+        solve(grid,row, col,i+1,j);
+        solve(grid,row, col,i,j-1);
+        solve(grid,row, col,i,j+1);
     }
     int numIslands(vector<vector<char>>& grid) {
-       
-        int n=grid.size();
-        int m=grid[0].size();
+        int rows=grid.size();
+        int cols=grid[0].size();
         int count=0;
-        for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-        if(grid[i][j]=='1'){
-            count++;
-        solve(i,j,grid,n,m);
-        }
-        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    solve(grid,rows,cols,i,j);
+                }
+            }
         }
         return count;
     }
