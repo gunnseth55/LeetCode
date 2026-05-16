591f450e2de407e4b8446951e472795ec12e3fd0
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2026-05-16 12:09:30 +0530

    Time: 13 ms (50.58%) | Memory: 18.8 MB (7.58%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,36 +7,34 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- bool find_ancestor( TreeNode *root, TreeNode *curr, vector<TreeNode*> & arr){
-    if(root==NULL){
-        return false;
-    }
-    arr.push_back(root);
-    if(root==curr){
-        return true ;
-    }
-     if(find_ancestor(root->left, curr, arr)||find_ancestor(root->right,curr,arr))
-     return true;
-
-     arr.pop_back();
-    return false;
- }
 class Solution {
 public:
+    bool find(TreeNode *root, TreeNode *target, vector<TreeNode*>&vec){
+        if(root==NULL)return false;
+        vec.push_back(root);
+        if(root==target)return true;
+       
+        if(find(root->left,target,vec)||find(root->right, target, vec)){
+            return true;
+        }
+        vec.pop_back();
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector <TreeNode*> one;
-        vector <TreeNode*> two;
+        vector<TreeNode*>one;
+        vector<TreeNode*>two;
         TreeNode *res=nullptr;
-        if(find_ancestor(root, p, one) && find_ancestor(root,q,two)){
+        if(find(root,p, one)&& find(root,q,two)){
             int n=min(one.size(),two.size());
-            for(int i=0;i<n;i++){
+                for(int i=0;i<n;i++){
                     if(one[i]==two[i]){
                         res=one[i];
                     }else{
                         break;
                     }
+                }
             }
-        }
+        
         return res;
     }
 };
