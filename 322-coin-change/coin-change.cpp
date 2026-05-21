3cc3f1cb1881eac542f5c3dffac478fe61e1c4d4
Gunn Seth <157601196+gunnseth55@users.noreply.github.com>
2026-05-21 19:22:47 +0530

    Time: 52 ms (19.47%) | Memory: 49.9 MB (18.86%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-long solve(int index,vector<int> &coins, int amount,vector<vector<int>>&dp){
-           
-            if(amount==0)return 0;
-            if(index<0)return 1e9;
-             if(dp[index][amount]!=-1)return dp[index][amount];
-            int take=1e9;
-            if(coins[index]<=amount){
-                take=1+solve(index,coins,amount-coins[index],dp);
-            }
-            int notake=solve(index-1,coins,amount,dp);
-            return dp[index][amount]=min(notake,take);
-        }
+    int solve(vector<int>& coins, int amount, vector<vector<int>>&dp, int index)
+    {
 
+        if(amount==0)return 0;
+        if(index<0)return 1e9;
+        if(dp[index][amount]!=-1)return dp[index][amount];
+        int take=1e9;
+        if(coins[index]<=amount){
+            take=1+solve(coins,amount-coins[index],dp,index);
+        }
+        int notTaken=solve(coins, amount, dp, index-1);
+        return dp[index][amount]=min(take,notTaken);
+
+
+    }
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(),coins.end());
-        int n=coins.size();
+        int n =coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int count=solve(n-1,coins,amount,dp);
-        return count>=1e9?-1:count;
+        int ans= solve(coins,amount, dp, n-1);
+        return ans>=1e9?-1:ans;
     }
 };
